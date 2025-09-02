e56e4809d6ececdf9b66bc8c8eaafddb2ca78242
JCRVZW <joaop08020@gmail.com>
2025-09-02 20:54:36 -0300

    Primeiro commit

--- tarefas.py
+++ tarefas.py
@@ -1,21 +1,19 @@
-numero = int (input("Informe um número inteiro ->"))
-lado_quadrado = int (input("diga o valor de um lado do quadrado ->"))
-idade = int (input("Informe quantos anos de idade tu tens ->"))
+numero = int (input("Informe um número inteiro que resultará em seu dobro -> "))
+print ("O dobro de",numero,"é -> ",numero*2)
+
+lado_quadrado = int (input("diga o valor de um lado do quadrado para obter sua área -> "))
+print ("A área de ",lado_quadrado,  "-> " , lado_quadrado*lado_quadrado)
+
+idade = int (input("Para saber seus meses e dias informe quantos anos de idade tu tens -> "))
 meses = idade*12
 dias = meses*365
-
-numero_media_1 = int (input("Diga um número"))
-numero_media_2 = int (input("Diga um número"))
-numero_media_3 = int (input("Diga um número"))
-media = numero_media_1 + numero_media_2 +numero_media_3/3
-
-
-
-
-
-
-
-print ("O dobro desse número é",numero*2)
-print ("A área é" , lado_quadrado*lado_quadrado)
 print ("tu tens",meses, "meses e",dias, "dias!")
-print ("a media dos números",numero_media_1,",",numero_media_2"e" ,numero_media_3, "é ->",media)
+
+numero_media_1 =  float(input("Agora calcularemos a média de três números, diga o número um -> "))
+numero_media_2 =  float(input("Informe o número dois -> "))
+numero_media_3 =  float(input("Insira o número três -> "))
+media = (numero_media_1 + numero_media_2 + numero_media_3) / 3
+print ("a media dos números",numero_media_1,",",numero_media_2, "e" ,numero_media_3, "é -> ",media,)
+
+
+
