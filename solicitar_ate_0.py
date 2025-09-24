ff0e101ce8a827aa5910f59f3895b05823211793
JCRVZW <mikesleydias@gmail.com>
2025-09-24 20:32:24 -0300

    ajuste repo

--- solicitar_ate_0.py
+++ solicitar_ate_0.py
@@ -8,6 +8,6 @@ print ("YOU ROCK MAN!!!")
 
 numero = 1
 while numero != 0:
-    numero = input("informe um numero -> ")
+    numero = int(input("informe um numero -> "))
 
 print ("YOU ROCK MAN!!!")
